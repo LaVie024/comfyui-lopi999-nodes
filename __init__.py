0041345825dec259409e620e1d82420f9885c7c7
LaVie024 <62406970+LaVie024@users.noreply.github.com>
2025-04-24 06:30:46 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,19 +2,22 @@ from .lopi999_utils import RandomSDXLLatentSize
 from .lopi999_utils import RandomNormalDistribution
 from .lopi999_utils import AdvancedTextSwitch
 from .lopi999_utils import ZipfSchedulerNode
+from .lopi999_utils import ZetaSchedulerNode
 
 NODE_CLASS_MAPPINGS = {
     "RandomSDXLLatentSize": RandomSDXLLatentSize,
     "RandomNormalDistribution": RandomNormalDistribution,
     "AdvancedTextSwitch": AdvancedTextSwitch,
-    "ZipfSchedulerNode": ZipfSchedulerNode
+    "ZipfSchedulerNode": ZipfSchedulerNode,
+    "ZetaSchedulerNode": ZetaSchedulerNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RandomSDXLLatentSize": "Random SDXL Latent Size",
     "RandomNormalDistribution": "Random Normal Distribution",
     "AdvancedTextSwitch": "Advanced Text Switch",
-    "ZipfSchedulerNode": "Zipf Scheduler"
+    "ZipfSchedulerNode": "Zipf Scheduler",
+    "ZetaSchedulerNode": "Zeta Scheduler"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
