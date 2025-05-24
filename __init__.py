71196f8a63b1f8d7afd09d828ba4e57d6bccbd49
LaVie024 <62406970+LaVie024@users.noreply.github.com>
2025-05-24 19:42:57 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,36 +1,34 @@
-from .lopi999_utils import RandomSDXLLatentSize
-from .lopi999_utils import RandomNormalDistribution
-from .lopi999_utils import AdvancedTextSwitch
-from .lopi999_utils import ZipfSchedulerNode
-from .lopi999_utils import ZetaSchedulerNode
-from .lopi999_utils import SDXLEmptyLatentSizePicker_v2
-from .lopi999_utils import Lopi999InputParameters
-from .lopi999_utils import ModelParameters
+import importlib
+import inspect
+import re
+
+lopi_utils = importlib.import_module(".lopi999_utils", package=__package__)
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+def camel_to_title(name):
+    return re.sub(
+        r'(?<=[a-z0-9])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])',
+        ' ',
+        name
+    ).replace('_', ' ')
+
+for name, cls in inspect.getmembers(lopi_utils, inspect.isclass):
+    if name.startswith("node_"):
+        node_key = name[5:]
+        NODE_CLASS_MAPPINGS[node_key] = cls
+        display_name = camel_to_title(node_key)
+        NODE_DISPLAY_NAME_MAPPINGS[node_key] = display_name
+
+from .lopi999_img import ImgBurn
+from .lopi999_img import HueSaturationLightLevels
+NODE_CLASS_MAPPINGS["BurnImage"] = ImgBurn
+NODE_CLASS_MAPPINGS["HueSaturationLightLevels"] = HueSaturationLightLevels
+NODE_DISPLAY_NAME_MAPPINGS["BurnImage"] = "Burn Image"
+NODE_DISPLAY_NAME_MAPPINGS["HueSaturationLightLevels"] = "Hue Saturation Light Levels"
 
 from . import sampler_list
-from .lopi999_utils import register_custom_samplers
-register_custom_samplers(sampler_list)
+lopi_utils.register_custom_samplers(sampler_list)
 
-NODE_CLASS_MAPPINGS = {
-    "RandomSDXLLatentSize": RandomSDXLLatentSize,
-    "RandomNormalDistribution": RandomNormalDistribution,
-    "AdvancedTextSwitch": AdvancedTextSwitch,
-    "ZipfSchedulerNode": ZipfSchedulerNode,
-    "ZetaSchedulerNode": ZetaSchedulerNode,
-    "SDXLEmptyLatentSizePicker_v2": SDXLEmptyLatentSizePicker_v2,
-    "Lopi999InputParameters": Lopi999InputParameters,
-    "ModelParameters": ModelParameters,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "RandomSDXLLatentSize": "Random SDXL Latent Size",
-    "RandomNormalDistribution": "Random Normal Distribution",
-    "AdvancedTextSwitch": "Advanced Text Switch",
-    "ZipfSchedulerNode": "Zipf Scheduler",
-    "ZetaSchedulerNode": "Zeta Scheduler",
-    "SDXLEmptyLatentSizePicker_v2": "SDXL Empty Latent Size Picker v2",
-    "Lopi999InputParameters": "Input Parameters (lopi999)",
-    "ModelParameters": "Model Parameters",
-}
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
